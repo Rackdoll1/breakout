8345e69fb526fbe61b9711def0c73fa667667d35
Andres Espino <josejos32@gmail.com>
2021-03-17 21:02:19 -0600

    The Entry Update
    Added a high score name entry screen.

---
+++ breakout-11/src/states/EnterHighScoreState.lua
@@ -0,0 +1,121 @@
+--[[
+    CS50G
+    Breakout Remake
+
+    -- EnterHighScoreState Class --
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Screen that allows us to input a new high score in the form of three characters, arcade-style.
+]]
+
+EnterHighScoreState = Class{__includes = BaseState}
+
+-- individual chars of our string
+local chars = {
+	[1] = 65,
+	[2] = 65,
+	[3] = 65
+}
+
+-- char we´re currently changing
+local highlightedChar = 1
+
+function EnterHighScoreState:enter(params)
+	self.highScores = params.highScores
+	self.score = params.score
+	self.scoreIndex = params.scoreIndex
+end
+
+function EnterHighScoreState:update(dt)
+	if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
+		-- update scores table
+		local name = string.char(chars[1])..string.char(chars[2])..string.char(chars[3])
+
+		-- go backwards through high scores table until this score, shifting scores
+		for i = 10, self.scoreIndex, -1 do
+			self.highScores[i + 1] = {
+				name = self.highScores[i].name,
+				score = self.highScores[i].score
+			}
+		end
+
+		self.highScores[self.scoreIndex].name = name
+		self.highScores[self.scoreIndex].score = self.score
+
+		-- write scores to file
+		local scoresStr = ''
+
+		for i = 1, 10 do
+			scoresStr = scoresStr..self.highScores[i].name..'\n'
+			scoresStr = scoresStr..tostring(self.highScores[i].score)..'\n'
+		end
+
+		love.filesystem.write('breakoutSave.lst', scoresStr)
+
+		gStateMachine:change('high-scores', {
+			highScores = self.highScores
+		})
+	end
+
+	-- scroll through character slots
+	if love.keyboard.wasPressed('left') and highlightedChar > 1 then
+		highlightedChar = highlightedChar - 1
+		gSounds['pause']:stop()
+		gSounds['pause']:play()
+	elseif love.keyboard.wasPressed('right') and highlightedChar < 3 then
+		highlightedChar = highlightedChar + 1
+		gSounds['pause']:stop()
+		gSounds['pause']:play()
+	end
+
+	-- scroll through characters
+	if love.keyboard.wasPressed('up') then
+		chars[highlightedChar] = chars[highlightedChar] + 1
+		gSounds['select']:stop()
+		gSounds['select']:play()
+		if chars[highlightedChar] > 90 then
+			chars[highlightedChar] = 65
+		end
+	elseif love.keyboard.wasPressed('down') then
+		chars[highlightedChar] = chars[highlightedChar] - 1
+		gSounds['select']:stop()
+		gSounds['select']:play()
+		if chars[highlightedChar] < 65 then
+			chars[highlightedChar] = 90
+		end
+	end
+end
+
+function EnterHighScoreState:render()
+	love.graphics.setFont(gFonts['medium'])
+	love.graphics.printf('Your score: '..tostring(self.score), 0, 30, VIRTUAL_WIDTH, 'center')
+
+	love.graphics.setFont(gFonts['large'])
+	-- render all three characters of the name
+	if highlightedChar == 1 then
+		love.graphics.setColor(103/255, 1, 1, 1)
+	end
+	love.graphics.print(string.char(chars[1]), VIRTUAL_WIDTH / 2 - 28, VIRTUAL_HEIGHT / 3 + 20)
+	love.graphics.setColor(1, 1, 1, 1)
+
+	if highlightedChar == 2 then
+		love.graphics.setColor(103/255, 1, 1, 1)
+	end
+	love.graphics.print(string.char(chars[2]), VIRTUAL_WIDTH / 2 - 6, VIRTUAL_HEIGHT / 3 + 20)
+	love.graphics.setColor(1, 1, 1, 1)
+
+	if highlightedChar == 3 then
+		love.graphics.setColor(103/255, 1, 1, 1)
+	end
+	love.graphics.print(string.char(chars[3]), VIRTUAL_WIDTH / 2 + 16, VIRTUAL_HEIGHT / 3 + 20)
+	love.graphics.setColor(1, 1, 1, 1)
+
+	love.graphics.setFont(gFonts['small'])
+	love.graphics.printf('Press Enter to confirm!', 0, VIRTUAL_HEIGHT - 18,
+		VIRTUAL_WIDTH, 'center')
+end
+
+
+
